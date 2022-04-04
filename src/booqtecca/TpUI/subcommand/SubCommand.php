fe8ce430f8843cd9055fe41cd1642f17c149bd3e
Booqtecca <80653401+Booqtecca@users.noreply.github.com>
2022-04-04 09:25:08 -0400

    Create SubCommand.php

---
+++ src/booqtecca/TpUI/subcommand/SubCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace booqtecca\TpUI\subcommand;
+
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\Plugin;
+
+abstract class SubCommand
+{
+    /** @var Plugin */
+    private $plugin;
+
+    /**
+     * @param Plugin $plugin
+     */
+    public function __construct(Plugin $plugin)
+    {
+        $this->plugin = $plugin;
+    }
+
+    /**
+     * @return Plugin
+     */
+    final public function getPlugin(): Plugin
+    {
+        return $this->plugin;
+    }
+
+    /**
+     * @param CommandSender $sender
+     * @return bool
+     */
+    abstract public function canUse(CommandSender $sender): bool;
+
+    /**
+     * @return string
+     */
+    abstract public function getUsage(): string;
+
+    /**
+     * @return string
+     */
+    abstract public function getName(): string;
+
+    /**
+     * @return string
+     */
+    abstract public function getDescription(): string;
+
+    /**
+     * @return string[]
+     */
+    abstract public function getAliases(): array;
+
+    /**
+     * @param CommandSender $sender
+     * @param string[] $args
+     * @return bool
+     */
+    abstract public function execute(CommandSender $sender, array $args): bool;
+}
